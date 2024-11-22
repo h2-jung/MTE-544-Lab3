c10b3e0ad8c39f44e7a5e31b98dc5667aee811e5
h2-jung <junghank122@gmail.com>
2024-11-21 23:38:05 -0500

    update plotting

--- plot_errors.py
+++ plot_errors.py
@@ -19,17 +19,24 @@ def plot_errors(filename):
     
     fig, axes = plt.subplots(2,1, figsize=(14,6))
 
+    plt.subplots_adjust(hspace=0.4)
 
-    axes[0].plot([lin[len(headers) - 5] for lin in values], [lin[len(headers) - 4] for lin in values])
-    axes[0].set_title("state space")
-    axes[0].grid()
-
+    ##### plotting for point with extra odom varaibles #####
+    # axes[0].plot([lin[len(headers) - 5] for lin in values], [lin[len(headers) - 4] for lin in values])
     
-    axes[1].set_title("each individual state")
+    axes[0].plot([lin[len(headers) - 3] for lin in values], [lin[len(headers) - 2] for lin in values])
+    axes[0].set_title("State Space")
+    axes[0].set_xlabel("State Variable X (m)")
+    axes[0].set_ylabel("State Variable Y (m)")
+    axes[0].grid()
+    
+    axes[1].set_title("Each Individual State")
+    axes[1].set_xlabel("Time (ns)")
+    axes[1].set_ylabel("State Variable Magnitudes")
     for i in range(0, len(headers) - 1):
         axes[1].plot(time_list, [lin[i] for lin in values], label= headers[i])
 
-    axes[1].legend()
+    axes[1].legend(bbox_to_anchor=(1.12, 0), loc='lower right')
     axes[1].grid()
 
     plt.show()
